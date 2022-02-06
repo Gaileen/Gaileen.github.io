3250be47a21ec6dca231caa02bc10f48da839d97
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-06 16:19:02 -0500

    Restart
    more responsive navbar

---
+++ scripts/headerscript.js
@@ -0,0 +1,9 @@
+﻿$(window).resize(function () {
+    if ($(this).width() < 992) {
+        $(".collapsible").hide();
+        $(".navbar-brand").addClass("mx-auto");
+
+    } else {
+        $(".collapsible").show();
+    }
+});
