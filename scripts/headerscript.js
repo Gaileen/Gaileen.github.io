e4b19e8b1a037c4d67d2290f5d248f220385c025
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-06 19:00:38 -0500

    changed index.html

--- scripts/headerscript.js
+++ scripts/headerscript.js
@@ -1,8 +1,17 @@
-﻿$(window).resize(function () {
-    if ($(this).width() < 992) {
+﻿function reformat(){
+    if ($(window).width() <= 992) { //992 slightly off
         $(".collapsible").hide();
         $(".navbar-brand").addClass("mx-auto");
+    } else {
+        $(".collapsible").show();
+    }
+}
+reformat(); // at the start of loading page, reformat nav for mobile
 
+$(window).resize(function () {
+    if ($(this).width() <= 992) { //992 slightly off
+        $(".collapsible").hide();
+        $(".navbar-brand").addClass("mx-auto");
     } else {
         $(".collapsible").show();
     }
