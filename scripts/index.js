181ae1ada159d4f6bbb567282bbbad468fbfb95b
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-12 23:14:00 -0500

    updated scripts for IG embeds

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,4 @@
-﻿function reformat() {
-    if ($(window).width() <= 992) {
-        if ($(window).width() <= 768) {
-            $("#slide-one").attr("src", "images/mobilebannercollage.png");
-        } else {
-            $("#slide-one").attr("src", "images/bannercollage.png");
-        }
-
-        $(".calendar").hide();
-        
-        $(".instagram-media").hide();
-        $("#mobile-post").show();
-        $("#ig-tag").removeClass("ps-5");
-    } else {
-
-        $(".calendar").show(); 
-
-        $(".instagram-media").show();
-        $("#ig-tag").addClass("ps-5");
-    }
-}
-reformat();
-
-$(window).resize(function () {
+﻿$(window).resize(function () {
     if ($(this).width() <= 992) {
         if ($(this).width() <= 768) {
             $("#slide-one").attr("src", "images/mobilebannercollage.png");
@@ -31,15 +8,13 @@ $(window).resize(function () {
 
         $(".calendar").hide();
         
-        $(".instagram-media").hide();
-        $("#mobile-post").show();
+        $(".older-posts").hide();
         $("#ig-tag").removeClass("ps-5");
     } else {
-        //$("#slide-one").attr("src", "images/bannercollage.png");
 
         $(".calendar").show();
 
-        $(".instagram-media").show();
+        $(".older-posts").show();
         $("#ig-tag").addClass("ps-5");
     }
 });
