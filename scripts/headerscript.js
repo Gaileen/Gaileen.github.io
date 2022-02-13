7328a25fc56200bd2e6d3e7935836df196675552
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-12 22:44:05 -0500

    moved reformat

--- scripts/headerscript.js
+++ scripts/headerscript.js
@@ -1,19 +1,19 @@
-﻿function reformat() {
-    if ($(window).width() <= 992) { 
-        $(".collapsible").hide();
-        alert("working..");
-        alert($(window).width());
-        $(".collapsible").css("color", "red");
-        $(".navbar-brand img").height(75);
-    } else {
-        alert("not working");
-        $(".collapsible").show();
+﻿//function reformat() {
+//    if ($(window).width() <= 992) { 
+//        $(".collapsible").hide();
+//        alert("working..");
+//        alert($(window).width());
+//        $(".collapsible").css("color", "red");
+//        $(".navbar-brand img").height(75);
+//    } else {
+//        alert("not working");
+//        $(".collapsible").show();
 
-        $(".navbar-brand img").width(130);
-        $(".navbar-brand img").height(130);
-    }
-}
-reformat(); // at the start of loading page, reformat nav for mobile
+//        $(".navbar-brand img").width(130);
+//        $(".navbar-brand img").height(130);
+//    }
+//}
+//reformat(); // at the start of loading page, reformat nav for mobile
 
 $(window).resize(function () {
     if ($(this).width() <= 992) { 
