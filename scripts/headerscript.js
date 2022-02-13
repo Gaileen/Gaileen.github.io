e8fa2948533c3fa9eb861de056259010e5ae77ed
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-12 22:08:01 -0500

    headerjs in header html

--- scripts/headerscript.js
+++ scripts/headerscript.js
@@ -1,9 +1,11 @@
 ﻿function reformat(){
     if ($(window).width() <= 992) { 
         $(".collapsible").hide();
-        //alert("working");
+        alert("working..");
+        $(".collapsible").css("color", "red");
         $(".navbar-brand img").height(75);
     } else {
+        alert("not working");
         $(".collapsible").show();
 
         $(".navbar-brand img").width(130);
@@ -19,8 +21,9 @@ $(window).resize(function () {
         $(".navbar-brand img").width(75);
         $(".navbar-brand img").height(75);
     } else {
+        alert("hi");
         $(".collapsible").show();
-
+        $(".collapsible").css("color", "red");
         $(".navbar-brand img").width(130);
         $(".navbar-brand img").height(130);
     }
