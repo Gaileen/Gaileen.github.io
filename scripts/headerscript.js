a732ed0ee5bb4756ee319e01e1c8f19905cead10
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-12 21:50:59 -0500

    script only in external html no work

--- scripts/headerscript.js
+++ scripts/headerscript.js
@@ -1,7 +1,7 @@
 ﻿function reformat(){
     if ($(window).width() <= 992) { 
         $(".collapsible").hide();
-        
+        //alert("working");
         $(".navbar-brand img").height(75);
     } else {
         $(".collapsible").show();
@@ -11,12 +11,10 @@
     }
 }
 reformat(); // at the start of loading page, reformat nav for mobile
-// ^^ THIS NO LONGER WORKS. SHOULD SEPARATE HTMLs REFERENCE THEIR SCRIPTS AS WELL?
 
 $(window).resize(function () {
     if ($(this).width() <= 992) { 
         $(".collapsible").hide();
-
         
         $(".navbar-brand img").width(75);
         $(".navbar-brand img").height(75);
