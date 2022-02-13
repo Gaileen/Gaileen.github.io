759c655242b9ff900db78cc38e9c5b7fd35893cd
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-12 22:56:00 -0500

    cleaned up header scripts and test alerts

--- scripts/headerscript.js
+++ scripts/headerscript.js
@@ -1,30 +1,12 @@
-﻿//function reformat() {
-//    if ($(window).width() <= 992) { 
-//        $(".collapsible").hide();
-//        alert("working..");
-//        alert($(window).width());
-//        $(".collapsible").css("color", "red");
-//        $(".navbar-brand img").height(75);
-//    } else {
-//        alert("not working");
-//        $(".collapsible").show();
-
-//        $(".navbar-brand img").width(130);
-//        $(".navbar-brand img").height(130);
-//    }
-//}
-//reformat(); // at the start of loading page, reformat nav for mobile
-
-$(window).resize(function () {
+﻿$(window).resize(function () {
     if ($(this).width() <= 992) { 
         $(".collapsible").hide();
         
         $(".navbar-brand img").width(75);
         $(".navbar-brand img").height(75);
     } else {
-        alert("hi");
         $(".collapsible").show();
-        $(".collapsible").css("color", "red");
+        
         $(".navbar-brand img").width(130);
         $(".navbar-brand img").height(130);
     }
