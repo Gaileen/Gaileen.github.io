9f55499be45acd990709b69bafa769273fff15b6
Gaileen Andal <52047895+Gaileen@users.noreply.github.com>
2022-02-14 23:44:41 -0500

    testing widget with click

--- scripts/footerscript.js
+++ scripts/footerscript.js
@@ -10,6 +10,9 @@ $(".email").hover(function () {
     alert("hovering");
     $(this).css("color", "#0A59CA");
 }, function () {
-        alert("not hovering");
     $(this).css("color", "black");
+});
+
+$(".email").click(function () {
+    alert("yo");
 });
